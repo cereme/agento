385a0993727f28062fc84a9852793a4af088a217
yun <ceremebsy@gmail.com>
2020-08-10 12:39:17 +0900

    Resolve #17

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,45 @@
 const ChromeExtensionReloader  = require('webpack-chrome-extension-reloader');
 const CopyPlugin = require('copy-webpack-plugin');
 const path = require('path');
+const archiver = require('archiver');
+const fs = require('fs');
+
+// Customized from https://github.com/Jasmin2895/extension-build-webpack-plugin
+class ExtensionDistBundler {
+    apply(compiler) {
+        compiler.hooks.afterEmit.tap("ExtensionDistBundler", ()=> {
+           let dirName = "dist";
+            fs.access(`./${dirName}`, (error) =>{
+                if (error) {
+                  console.log("Extension Plugin ERR: directory missing");
+                } else {
+                    this.createzipFile(dirName, "agento.bundle.zip");
+                }
+              })
+        })        
+    }
+    createzipFile(dirName, outputFileName) {
+        let output = fs.createWriteStream(outputFileName);
+        let archive = archiver("zip");
+
+        output.on("close", function(){
+            console.log(archive.pointer() + ' total bytes');
+            console.log('archiver has been finalized and the output file descriptor has closed.');
+        })
+
+        archive.on("error", function(err){
+            throw err;
+        })
+
+        archive.pipe(output);
+
+        archive.directory(`${dirName}/`, false);
+        archive.file("manifest.json");
+        console.log(`${dirName}.zip file created in root directory`);
+        
+        archive.finalize();
+    }
+}
 
 let config = {
     entry: {
@@ -44,7 +83,9 @@ module.exports = (env, args) => {
     }
 
     if(args.mode === "production"){
-        //TODO: build dist for prod
+        config.plugins.push(
+            new ExtensionDistBundler()
+        );
     }
 
     return config;
